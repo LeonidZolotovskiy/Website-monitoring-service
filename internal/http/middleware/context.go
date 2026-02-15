ca68795e3c511afa58593f75eddef3b073daf0b1
Leonid Zolotovskiy <zolotovsky@innovasystem.pro>
2026-02-15 12:34:31 +0300

    #38: fix lint

--- internal/http/middleware/context.go
+++ internal/http/middleware/context.go
@@ -2,7 +2,6 @@ package middleware
 
 type ctxKey string
 
-const (
-	RequestIDKey ctxKey = "request_id"
-	RequestIDHeader     = "X-Request-ID"
-)
+const RequestIDKey ctxKey = "request_id"
+
+const RequestIDHeader = "X-Request-ID"
